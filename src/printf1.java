1a057b461e7e00133ebc1c8080a0d498d9c326b2
subhas <subhasqa88@gmail.com>
2025-09-14 11:05:31 +0530

    Merge remote-tracking branch 'origin/master'

---
+++ src/printf1.java
@@ -0,0 +1,27 @@
+public class printf1 {
+    public static void main(String[] args) {
+        int table=9;
+        System.out.printf("%d*1=%d",table,table *1);
+        System.out.println();
+        System.out.printf("%d*2=%d",table,table *2);
+        System.out.println();
+        System.out.printf("%d*3=%d",table,table *3);
+        System.out.println();
+        System.out.printf("%d*4=%d",table,table *4);
+        System.out.println();
+        System.out.printf("%d*5=%d",table,table *5);
+        System.out.println();
+        System.out.printf("%d*6=%d",table,table *6);
+        System.out.println();
+        System.out.printf("%d*7=%d",table,table *7);
+        System.out.println();
+        System.out.printf("%d*8=%d",table,table *8);
+        System.out.println();
+        System.out.printf("%d*9=%d",table,table *9);
+        System.out.println();
+        System.out.printf("%d*10=%d",table,table *10);
+
+
+
+    }
+}
