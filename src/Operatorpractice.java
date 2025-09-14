520617961f9ec4c6891a4bf60a44c10e802836fe
subhas <subhasqa88@gmail.com>
2025-09-14 11:10:26 +0530

    assignmentoperator

---
+++ src/Operatorpractice.java
@@ -0,0 +1,9 @@
+public class Operatorpractice {
+    public static void main(String[] args)
+    {
+        int a=10;
+        int b=34;
+        int sum=a+b;
+        System.out.println(sum);
+    }
+}
