63221fbe08af2c580d21139ca22098a99e98e4a8
subhas.das <subhas.das@tavant.com>
2025-09-14 10:22:59 +0530

    operator3

---
+++ src/Operator4.java
@@ -0,0 +1,6 @@
+public class Operator4
+{
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/operator3.java
@@ -0,0 +1,8 @@
+public class operator3 {
+    public static void main(String[] args) {
+        int a1=10;
+        int a2=30;
+        System.out.println(a1+a2);
+        System.out.println(a1);
+    }
+}
