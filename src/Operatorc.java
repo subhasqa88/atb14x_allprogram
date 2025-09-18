505e859681b1eee61842a103878f7b45b1426111
subhas <subhasqa88@gmail.com>
2025-09-18 22:29:33 +0530

    “Updated Operatorc to use num instead of color”)

--- src/Operatorc.java
+++ src/Operatorc.java
@@ -1,6 +1,6 @@
 public class Operatorc {
     public static void main(String[] args) {
-        int color=70;
-        System.out.println(color);
+        int num =70;
+        System.out.println(num);
     }
 }
