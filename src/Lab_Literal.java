61d061fef9e70f6ed91e9c55aaa0569ac7aa3c6b
subhas <subhasqa88@gmail.com>
2025-09-18 21:57:33 +0530

    Your commit message

---
+++ src/Lab_Literal.java
@@ -0,0 +1,20 @@
+public class Lab_Literal {
+    public static void main(String[] args) {
+        int age=20;
+        System.out.println(age);
+        float pi=2.2f;
+        System.out.println(pi);
+        final String name="subhas";
+        System.out.println(name);
+       float a =2.4f;
+       boolean c=true;
+       boolean is_tested=false;
+        System.out.println(c);
+        System.out.println(is_tested);
+        char ct='t';
+        System.out.println(ct);
+
+
+
+    }
+}
