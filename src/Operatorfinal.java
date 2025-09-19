13a86e0477dd1a651546d4b578f1f5cce6d88df0
subhas <subhasqa88@gmail.com>
2025-09-19 18:25:20 +0530

    Unaryopertorcommit

---
+++ src/Operatorfinal.java
@@ -0,0 +1,10 @@
+public class Operatorfinal
+{
+    public static void main(String[] args) {
+         int v=10;
+        System.out.println(v);
+        System.out.println(v++);
+        System.out.println(v);
+
+    }
+}
