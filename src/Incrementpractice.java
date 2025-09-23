19a3157599ade8cf144e1cb304d769d4f411049d
subhas.das <subhas.das@tavant.com>
2025-09-23 14:28:30 +0530

    Increment practice

---
+++ src/Incrementpractice.java
@@ -0,0 +1,9 @@
+public class Incrementpractice {
+    public static void main(String[] args) {
+        int num=10;
+        int num1=num++;
+        System.out.println(num1);
+        System.out.println(num++);
+        System.out.println(num++);
+    }
+}
