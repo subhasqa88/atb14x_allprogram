c080ece99fef4d30b35c9e62b5f9606ea6ecd680
subhas.das <subhas.das@tavant.com>
2025-09-23 14:36:04 +0530

    Incrementdecrement practice

---
+++ src/Incrementdecrement.java
@@ -0,0 +1,11 @@
+public class Incrementdecrement {
+    public static void main(String[] args) {
+        int age5=60;
+        int age6=age5++;
+        int age7=age5--;
+
+        System.out.println(age6);
+        System.out.println(age7);
+        System.out.println(age5);
+    }
+}
