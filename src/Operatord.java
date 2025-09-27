731f3d786fc3680751ba913fd844ca7a36ff3e0e
subhas.das <subhas.das@tavant.com>
2025-09-27 19:26:13 +0530

    Debuggerstepspractice push

---
+++ src/Operatord.java
@@ -0,0 +1,13 @@
+public class Operatord {
+    public static void main(String[] args) {
+        int g=100;
+        int i=20;
+        System.out.println(g+i);
+        System.out.println(g-i);
+        System.out.println(g/i);
+        System.out.println(g*i);
+        System.out.println(g%i);
+        //System.out.println(g++);
+        System.out.println(++g);
+    }
+}
