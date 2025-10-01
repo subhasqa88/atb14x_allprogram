c3e13b4b24203bc615afb0d9cd878a1db294fc14
subhas.das <subhas.das@tavant.com>
2025-10-01 19:13:06 +0530

    forloop practice

---
+++ src/Forlooppractice.java
@@ -0,0 +1,10 @@
+public class Forlooppractice {
+    public static void main(String[] args) {
+        int i=10;
+        for(int j=0;j<=50;j++)
+        {
+            System.out.println(i++);
+
+        }
+    }
+}
