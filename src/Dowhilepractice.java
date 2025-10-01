644c6ed994d339921fca81286ee02ebe4512b6d8
subhas.das <subhas.das@tavant.com>
2025-10-01 19:47:08 +0530

    practice

---
+++ src/Dowhilepractice.java
@@ -0,0 +1,10 @@
+public class Dowhilepractice {
+     static int a=10;
+    public static void main(String[] args) {
+        do{
+            System.out.println("Dowhile loop executed once atleast// or executed ");
+            a++;
+            System.out.println(a);
+        }while(a<20);
+    }
+}
